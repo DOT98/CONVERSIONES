b8a6551584bf975416a7f916f3179173e5db66e4
Andres Jurado <anmijurane@gmail.com>
2020-06-03 10:32:38 -0500

    Control de las conversiones con ejemplo

--- conversiones/src/Working/tutorial.java
+++ conversiones/src/Working/tutorial.java
@@ -5,6 +5,8 @@
  */
 package Working;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author danie
@@ -32,11 +34,12 @@ public class tutorial extends javax.swing.JFrame {
         jTextArea1 = new javax.swing.JTextArea();
         jButton2 = new javax.swing.JButton();
         btn_inicio = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        setOp = new javax.swing.JTextField();
+        resConv = new javax.swing.JTextField();
+        cmbx_1 = new javax.swing.JComboBox<>();
+        jT_Indications = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        cmbx_2 = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -51,6 +54,7 @@ public class tutorial extends javax.swing.JFrame {
         });
         getContentPane().add(btn_ejercicios, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 290, -1, -1));
 
+        jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setLineWrap(true);
         jTextArea1.setRows(5);
@@ -61,7 +65,12 @@ public class tutorial extends javax.swing.JFrame {
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 190, 320));
 
         jButton2.setText("Convertir");
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 160, -1, 40));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 170, -1, 40));
 
         btn_inicio.setText("Inicio");
         btn_inicio.addActionListener(new java.awt.event.ActionListener() {
@@ -71,30 +80,35 @@ public class tutorial extends javax.swing.JFrame {
         });
         getContentPane().add(btn_inicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 290, -1, -1));
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        setOp.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                setOpActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 100, 60, -1));
+        getContentPane().add(setOp, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 100, 60, -1));
 
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        resConv.setEditable(false);
+        resConv.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                resConvActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 100, 60, -1));
+        getContentPane().add(resConv, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 100, 110, -1));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KM", "M", "CM", "DM", "MM" }));
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 100, -1, -1));
+        cmbx_1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KM", "M", "CM", "MM" }));
+        getContentPane().add(cmbx_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 100, 60, -1));
+
+        jT_Indications.setEditable(false);
+        jT_Indications.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(jT_Indications, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 140, 370, -1));
 
         jLabel2.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 204, 0));
         jLabel2.setText("a:");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 90, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 90, 20, -1));
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KM", "M", "CM", "DM", "MM" }));
-        getContentPane().add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 100, -1, -1));
+        cmbx_2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KM", "M", "CM", "MM" }));
+        getContentPane().add(cmbx_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 100, 60, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Working/tutorial.jpg"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 610, 360));
@@ -103,13 +117,13 @@ public class tutorial extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void setOpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_setOpActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_setOpActionPerformed
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void resConvActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resConvActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_resConvActionPerformed
 
     private void btn_ejerciciosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_ejerciciosActionPerformed
         ejercicios acc = new ejercicios();
@@ -122,6 +136,129 @@ public class tutorial extends javax.swing.JFrame {
         acc.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btn_inicioActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        String a = (String) cmbx_1.getSelectedItem();
+        String b = (String) cmbx_2.getSelectedItem();
+        double v = cmbx_2.getSelectedIndex();
+        System.out.println(a);
+        System.out.println(v);
+
+        
+        switch (cmbx_1.getSelectedIndex()) {
+            case 0://KM
+                switch (b) {
+                    case "KM":
+                        JOptionPane.showMessageDialog(this, "Es la misma medida");                        
+                        break;
+                    case "M":
+                        double m = Double.parseDouble(setOp.getText());
+                        m = m * 1000;
+                        resConv.setText(""+m); 
+                        jT_Indications.setText("multiplica el valor de longitud por 1000");
+                        break;
+                    case "CM":
+                        double cm = Double.parseDouble(setOp.getText());
+                        cm = cm * 100000;
+                        resConv.setText(""+cm);
+                        jT_Indications.setText("multiplica el valor de longitud por 100000");
+                        break;
+                    case "MM":
+                        double mm = Double.parseDouble(setOp.getText());
+                        mm = mm * 1000000;
+                        resConv.setText(""+mm);
+                        jT_Indications.setText("multiplica el valor de longitud por 1000000");
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                break;
+            case 1://M
+                switch (b) {
+                    case "KM":
+                        double km = Double.parseDouble(setOp.getText());
+                        km = km / 1000;
+                        resConv.setText(""+km);
+                        jT_Indications.setText("divide el valor de longitud entre 1000");
+                        break;
+                    case "M":
+                        JOptionPane.showMessageDialog(this, "Es la misma medida");
+                        break;
+                    case "CM":
+                        double cm = Double.parseDouble(setOp.getText());
+                        cm = cm * 100;
+                        resConv.setText(""+cm);
+                        jT_Indications.setText("multiplica el valor de longitud por 100");
+                        break;
+                    case "MM":
+                        double mm = Double.parseDouble(setOp.getText());
+                        mm = mm * 1000;
+                        resConv.setText(""+mm);
+                        jT_Indications.setText("multiplica el valor de longitud por 1000");
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                break;
+            case 2://CM
+                switch (b) {
+                    case "KM":
+                        double km = Double.parseDouble(setOp.getText());
+                        km = km / 100000;
+                        resConv.setText(""+km);
+                        jT_Indications.setText("divide el valor de longitud entre 100000");
+                        break;
+                    case "M":
+                        double m = Double.parseDouble(setOp.getText());
+                        m = m / 100;
+                        resConv.setText(""+m);
+                        jT_Indications.setText("divide el valor de longitud entre 100");
+                        break;
+                    case "CM":
+                        JOptionPane.showMessageDialog(this, "Es la misma medida");
+                        break;
+                    case "MM":
+                        double mm = Double.parseDouble(setOp.getText());
+                        mm = mm * 10;
+                        resConv.setText(""+mm);
+                        jT_Indications.setText("multiplica el valor de longitud por 10");
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                break;
+            case 3://MM
+                switch (b) {
+                    case "KM":
+                        double km = Double.parseDouble(setOp.getText());
+                        km = km / 1000000;
+                        resConv.setText(""+km);
+                        jT_Indications.setText("divide el valor de longitud entre 1e+6");
+                        break;
+                    case "M":
+                        double m = Double.parseDouble(setOp.getText());
+                        m = m * 1000;
+                        resConv.setText(""+m);
+                        jT_Indications.setText("divide el valor de longitud entre 1000");
+                        break;
+                    case "CM":
+                        double cm = Double.parseDouble(setOp.getText());
+                        cm = cm / 10;
+                        resConv.setText(""+cm);
+                        jT_Indications.setText("divide el valor de longitud entre 10");
+                        break;
+                    case "MM":
+                        JOptionPane.showMessageDialog(this, "Es la misma medida");
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -162,14 +299,15 @@ public class tutorial extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_ejercicios;
     private javax.swing.JButton btn_inicio;
+    private javax.swing.JComboBox<String> cmbx_1;
+    private javax.swing.JComboBox<String> cmbx_2;
     private javax.swing.JButton jButton2;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jT_Indications;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField resConv;
+    private javax.swing.JTextField setOp;
     // End of variables declaration//GEN-END:variables
 }
